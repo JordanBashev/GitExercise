c6b0054b7961e703edbe15748308e4c9ec9841a0
dani bashev <vbedeko@gmail.com>
2021-03-14 11:18:33 +0200

    Add project files.

---
+++ OptionsManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Console Calculator App");
+            Console.WriteLine(new string('-', 15));
+
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.WriteLine("Choose one from the listed options:");
+            foreach (string option in OptionsManager.OptionsList)
+            {
+                Console.WriteLine($"\t{option}");
+            }
+
+            Console.Write("Option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "s":
+                    OptionsManager.Subtract(a, b);
+                    break;
+                case "m":
+                    OptionsManager.Multiply(a, b);
+                    break;
+            }
+
+            Console.WriteLine("Pres any key to close the app...");
+            Console.ReadKey(true);
+        }
+    }
+}
